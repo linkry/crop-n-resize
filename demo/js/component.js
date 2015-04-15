959c9d14710e5094152c86305b6d1dbdaa3934a9
Youness MABROUK <youness.mabrouk@gmail.com>
2015-04-15 21:57:15 +0100

    upload

--- demo/js/component.js
+++ demo/js/component.js
@@ -3,57 +3,78 @@ var resizeableImage = function(options) {
   // Some variable and settings
     var $container,
         $overlay = document.querySelector(".component"),
+        $input = document.querySelector('#cnr-input'),
         img = {},
         orig_src = new Image(),
-        image_target = document.querySelector(options.target),
+        image_target = null,
         event_state = {},
-        constrain = false,
         min_width = options.width, // Change as required
         min_height = options.height,
         resize_canvas = document.createElement('canvas');
-        $overlay.style.width = options.width+"px";
-        $overlay.style.height = options.height+"px";
-    init = function(){
+    $overlay.style.width = options.width+"px";
+    $overlay.style.height = options.height+"px";
+
+    init = function(imgURI){
+
+        image_target = document.createElement("img");
+        image_target.onload = function() {
+            img.originalWidth = this.width;
+            img.originalHeight = this.height;
+            console.log("image dimensions",this.width, this.height);
+            resizeInitImage(min_width,min_height, this.width, this.height);
+        };
+        image_target.setAttribute("src",imgURI);
+
+        image_target.setAttribute("class","resize-image");
+
+        $container = document.createElement("div");
+        $container.setAttribute("class","resize-container");
+
+        $overlay.appendChild($container);
+        $container.appendChild(image_target);
+        //give it data uri from filereader
+
+        //
+
+
 
         // When resizing, we will always use this copy of the original as the base
         orig_src.src=image_target.src;
 
         // Wrap the image with the container and add resize handles
-        $(image_target).wrap('<div class="resize-container"></div>')// nojquery
+        //$(image_target).wrap('<div class="resize-container"></div>')// nojquery
         /*.before('<span class="resize-handle resize-handle-nw"></span>')
         .before('<span class="resize-handle resize-handle-ne"></span>')
         .after('<span class="resize-handle resize-handle-se"></span>')
         .after('<span class="resize-handle resize-handle-sw"></span>');*/
 
         // Assign the container to a variable
-        $container =  $(image_target).parent('.resize-container');// nojquery
+        //$container =  $(image_target).parent('.resize-container');// nojquery
 
         // Add events
-        $container.on('mousedown touchstart', 'img', startMoving);// nojquery
+        $($container).on('mousedown touchstart', 'img', startMoving);// nojquery
         $('.js-crop').on('click', crop);// nojquery
-        $overlay = $("#overlay");
         img.zoom = 0;
-        img.width = $("#overlay img").width();
-        img.height = $("#overlay img").height();
+        img.width = $(image_target).width();
+        img.height = $(image_target).height();
         img.boundaries = {};
-        img.boundaries.max = $overlay.offset();
+        img.boundaries.max = $($overlay).offset();
         img.boundaries.min = {
-            left : Math.round($overlay.offset().left - (img.width - $overlay.width())), 
-            top : Math.round($overlay.offset().top - (img.height - $overlay.height()))
+            left : Math.round($($overlay).offset().left - (img.width - $($overlay).width())), 
+            top : Math.round($($overlay).offset().top - (img.height - $($overlay).height()))
         };
-        resizeInitImage(min_width,min_height, img.width, img.height);
         document.getElementById("zoom").value = 0;
         img.ratio = 0;
         document.getElementById("zoom").addEventListener("change",zoom);
-
+        
     };
 
     saveEventState = function(e) {
         // Save the initial event details and container state
-        event_state.container_width = $container.width();
-        event_state.container_height = $container.height();
-        event_state.container_left = $container.offset().left; 
-        event_state.container_top = $container.offset().top;
+        event_state.container_width = $($container).width();
+        event_state.container_height = $($container).height();
+        event_state.container_left = $($container).offset().left; 
+        event_state.container_top = $($container).offset().top;
         event_state.mouse_x = (e.clientX || e.pageX || e.originalEvent.touches[0].clientX) + $(window).scrollLeft(); 
         event_state.mouse_y = (e.clientY || e.pageY || e.originalEvent.touches[0].clientY) + $(window).scrollTop();
 
@@ -76,7 +97,7 @@ var resizeableImage = function(options) {
         resize_canvas.width = img.width;
         resize_canvas.height = img.height;
 
-        offset = $container.offset();
+        offset = $($container).offset();
 
         var xleft, xtop, oldWidth, oldHeight, newWidth;
         newWidth = img.width;
@@ -98,8 +119,8 @@ var resizeableImage = function(options) {
 
         
         img.boundaries.min = {
-            left : Math.round($overlay.offset().left - (img.width - $overlay.width())), 
-            top : Math.round($overlay.offset().top - (img.height - $overlay.height()))
+            left : Math.round($($overlay).offset().left - (img.width - $($overlay).width())), 
+            top : Math.round($($overlay).offset().top - (img.height - $($overlay).height()))
         };
 
 
@@ -122,7 +143,7 @@ var resizeableImage = function(options) {
             xtop = img.boundaries.min.top;
         }
 
-        $container.offset({
+        $($container).offset({
             top : xtop,
             left : xleft
         }); 
@@ -132,9 +153,13 @@ var resizeableImage = function(options) {
     };
 
     resizeInitImage = function(width, height, oldWidth, oldHeight){
-
+        console.log(width, height, oldWidth, oldHeight);
         if(oldWidth < oldHeight) {
-            //todo
+            ratio = oldWidth/width;
+            resize_canvas.width = width;
+            resize_canvas.height = oldHeight/ratio;
+            img.height = oldHeight/ratio;
+            img.width =  width;
         } else {
             ratio = oldHeight/height;
             resize_canvas.width = oldWidth/ratio;
@@ -143,19 +168,17 @@ var resizeableImage = function(options) {
             img.width =  oldWidth/ratio;
         }
 
-        img.originalHeight = img.height;
-        img.originalWidth = img.width; 
-
         img.boundaries.min = {
-            left : Math.round($overlay.offset().left - (img.width - $overlay.width())), 
-            top : Math.round($overlay.offset().top - (img.height - $overlay.height()))
+            left : Math.round($($overlay).offset().left - (img.width - $($overlay).width())), 
+            top : Math.round($($overlay).offset().top - (img.height - $($overlay).height()))
         };
 
-        $container.offset({
+        $($container).offset({
             top : (img.boundaries.min.top + img.boundaries.max.top)/2,
             left : (img.boundaries.min.left + img.boundaries.max.left)/2
         });  
-
+        console.log(orig_src,img.width, img.height);
+        image_target.onload = null;
         resize_canvas.getContext('2d').drawImage(orig_src, 0, 0, img.width, img.height);   
         $(image_target).attr('src', resize_canvas.toDataURL("image/png")); // nojquery 
     };
@@ -209,7 +232,7 @@ var resizeableImage = function(options) {
             offset.top = img.boundaries.min.top;
         }
 
-        $container.offset({
+        $($container).offset({
             'left': offset.left,
             'top':  offset.top 
         });        
@@ -227,10 +250,10 @@ var resizeableImage = function(options) {
     crop = function(){
         //Find the part of the image that is inside the crop box
         var crop_canvas,
-            left = $overlay.offset().left - $container.offset().left,// nojquery
-            top =  $overlay.offset().top - $container.offset().top,// nojquery
-            width = $overlay.width(),// nojquery
-            height = $overlay.height();// nojquery
+            left = $($overlay).offset().left - $($container).offset().left,// nojquery
+            top =  $($overlay).offset().top - $($container).offset().top,// nojquery
+            width = $($overlay).width(),// nojquery
+            height = $($overlay).height();// nojquery
         	
         crop_canvas = document.createElement('canvas');
         crop_canvas.width = width;
@@ -241,16 +264,44 @@ var resizeableImage = function(options) {
         window.open(crop_canvas.toDataURL("image/png"));
     }
 
+    handleFileSelect = function(evt) {
+        var file=evt.currentTarget.files[0];
+        var reader = new FileReader();
+        if(file.type != "image/jpeg" && file.type != "image/png") {
+            //wrong type
+            reader.abort();
+            return;
+        }
 
+        if(file.size/1000000 > 10) {
+            //too big to handle
+            reader.abort();
+            return;
+        }
 
-    init();
+        reader.onError = function(error) {
+            console.error(error)
+            reader.abort();
+            return;
+        };
+        reader.onloadstart = function(ev) {
+        };
+        reader.onloadend = function(ev) {
+        };
 
+        reader.onload = function (evt) {
+            init(evt.target.result)
+        };
+        reader.readAsDataURL(file);
+    };
+
+    $input.addEventListener('change',handleFileSelect);
     window.crop = crop;
 };
 
 // Kick everything off with the target image
 resizeableImage({
-    target : '.resize-image',
+    target : '#image',
     width : 250,
     height : 200,
 });// nojquery
